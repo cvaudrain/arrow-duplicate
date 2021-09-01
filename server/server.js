a2c5fa64681231ee19437f5bc9f05be0218d681e
cvaudrain <cvaudrain@gmail.com>
2021-09-01 17:12:29 -0400

    save project before altering directory structure

--- server/server.js
+++ server/server.js
@@ -17,9 +17,9 @@ const DB_URI   = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS
 
 //Serve Production Build as static:
 // app.use(express.static(path.join(__dirname,"../client/build")));
-app.use(express.static(path.join(__dirname,"build")));
+app.use(express.static(path.join(__dirname,"./client/build")));
 app.get("*",(req,res)=>{
-   res.sendFile(path.join(__dirname,"build","index.html"))
+   res.sendFile(path.join(__dirname,".client/build","index.html"))
 })
 
 // Middleware
