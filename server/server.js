a3187c12645eba5ab3a0686e7ffc6a943ee20259
cvaudrain <cvaudrain@gmail.com>
2021-09-01 23:42:18 -0400

    plaintext secret as required

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ app.use(cors())
 app.use(session({
    resave:false,
    saveUninitialized:false,
-   secret: SECRET //REQUIRED here, not as a process.env
+   secret: "BlueElephant" //REQUIRED here, not as a process.env
 }))
 app.use(passport.initialize())
 app.use(passport.session())
